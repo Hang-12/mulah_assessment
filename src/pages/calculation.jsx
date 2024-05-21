6b09f89fd25d1d7860747d8b7c24647a9fc30008
Hang <sahzonghang@gmail.com>
2024-05-21 22:14:00 +0800

    first commit

---
+++ src/pages/calculation.jsx
@@ -0,0 +1,15 @@
+import { datas } from './data/data';
+
+const findValue = (index) => {
+  return datas.find((item) => item.index === index).value;
+}
+
+const alpha = findValue('A5') + findValue('A20');
+const beta = findValue('A15') / findValue('A7');
+const charlie = findValue('A13') * findValue('A12'); 
+
+export const category = [
+  { label: `Alpha`, total: alpha },
+  { label: `Beta`, total: beta },
+  { label: `Charlie`, total: charlie },
+];
---
+++ src/pages/index.jsx
@@ -0,0 +1,70 @@
+import Head from "next/head";
+import { datas } from "./data/data";
+import { category } from "./calculation";
+
+const Home = () => {
+  return (
+    <>
+      <Head>
+        <title>Mulah Assessment</title>
+        <meta name="description" content="Mulah Assessment" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className="bg-white min-h-screen p-4">
+        <div className="p-2">
+          <p className="text-3xl font-bold">Table 1</p>
+          <ul className="divide-y m-2">
+            <li className="py-4 grid grid-cols-2">
+              <div className="col-span-1">
+                <p className="font-bold">{`Index #`}</p>
+              </div>
+              <div className="col-span-1">
+                <p className="font-bold">{`Value`}</p>
+              </div>
+            </li>
+            {datas.map((data) => {
+              return (
+                <li key={data.index} className="py-2 grid grid-cols-2">
+                  <div className="col-span-1">
+                    <p>{data.index}</p>
+                  </div>
+                  <div className="col-span-1">
+                    <p>{data.value}</p>
+                  </div>
+                </li>
+              )
+            })}
+          </ul>
+        </div>
+        <div className="p-2">
+          <p className="text-3xl font-bold">Table 2</p>
+          <ul className="divide-y m-2">
+            <li className="py-4 grid grid-cols-2">
+              <div className="col-span-1">
+                <p className="font-bold">{`Category`}</p>
+              </div>
+              <div className="col-span-1">
+                <p className="font-bold">{`Value`}</p>
+              </div>
+            </li>
+            {category.map((result) => {
+              return (
+                <li key={result.label} className="py-2 grid grid-cols-2">
+                  <div className="col-span-1">
+                    <p>{result.label}</p>
+                  </div>
+                  <div className="col-span-1">
+                    <p>{result.total}</p>
+                  </div>
+                </li>
+              )
+            })}
+          </ul>
+        </div>
+      </main>
+    </>
+  )
+}
+
+export default Home;
